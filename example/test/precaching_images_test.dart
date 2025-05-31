09ae7551a2a3e4650e7be5286e86ea9a0ab37222
lilatee <marcin.hradowicz@gmail.com>
2025-05-31 10:49:57 +0200

    teat: add test for precaching images

---
+++ example/test/precaching_images_test.dart
@@ -0,0 +1,46 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:golden_test/golden_test.dart';
+
+final red100x100PixelsImage = base64Decode(
+  'iVBORw0KGgoAAAANSUhEUgAAAGQAAABkCAYAAABw4pVUAAAApElEQVR42u3RAQ0AAAjD'
+  'MO5fNCCDkC5z0HTVrisFCBABASIgQAQEiIAAAQJEQIAICBABASIgQAREQIAICBABASIg'
+  'QAREQIAICBABASIgQAREQIAICBABASIgQAREQIAICBABASIgQAREQIAICBABASIgQARE'
+  'QIAICBABASIgQAREQIAICBABASIgQAREQIAICBABASIgQAQECBAgAgJEQIAIyPcGFY7H'
+  'nV2aPXoAAAAASUVORK5CYII=',
+);
+
+void main() {
+  goldenTest(
+    name: 'Image from asset should be displayed',
+    builder: (_) => Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          const Image(
+            image: AssetImage('assets/images/flutter_logo.png'),
+            height: 100,
+            width: 100,
+          ),
+          FadeInImage(
+            image: AssetImage('assets/images/flutter_logo.png'),
+            placeholder: MemoryImage(red100x100PixelsImage),
+            height: 100,
+            width: 100,
+          ),
+
+          /// TODO: Mock network image.
+          // const DecoratedBox(
+          //   decoration: BoxDecoration(
+          //     image: DecorationImage(
+          //       image: NetworkImage('mockURL'),
+          //     ),
+          //   ),
+          // ),
+        ],
+      ),
+    ),
+    supportMultipleDevices: true,
+  );
+}
